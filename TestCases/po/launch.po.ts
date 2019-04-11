cc7b9ad7fc01a461b05ceac2c474bacba1a9e7cf
wziyuan <wziyuan@vmware.com>
2019-04-11 17:10:15 +0800

    change tab to 3 spaces

--- TestCases/po/launch.po.ts
+++ TestCases/po/launch.po.ts
@@ -3,31 +3,31 @@ import { LoginPage } from './login.po'
 
 export class LaunchPage {
 
-    launchUrl: string = browser.baseUrl + '/portal/webclient/index.html#/launchitems'
+   launchUrl: string = browser.baseUrl + '/portal/webclient/index.html#/launchitems'
 
-    defaultItem: string = 'win2019'
+   defaultItem: string = 'win2019'
 
-    loginPage: LoginPage
+   loginPage: LoginPage
 
-    navigateTo() {
-        // first get login page, then login with valid user credential.
-        this.loginPage = new LoginPage()
-        this.loginPage.navigateTo()
-        this.loginPage.Login(this.loginPage.aValidUser)
-    }
+   navigateTo() {
+      // first get login page, then login with valid user credential.
+      this.loginPage = new LoginPage()
+      this.loginPage.navigateTo()
+      this.loginPage.Login(this.loginPage.aValidUser)
+   }
 
-    getPageIdText() {   // equals to 'Search'.
-        let searchBtnElm = element(by.css('label[for="header-search"]'))
-        let idText: string
-        searchBtnElm.waitReady().then(() => {
-            idText = searchBtnElm.getAttribute('textContent').toString()
-        })
-        return idText
-    }
+   getPageIdText() {   // equals to 'Search'.
+      let searchBtnElm = element(by.css('label[for="header-search"]'))
+      let idText: string
+      searchBtnElm.waitReady().then(() => {
+         idText = searchBtnElm.getAttribute('textContent').toString()
+      })
+      return idText
+   }
 
-    // searchItem(name: string | null) {
-    //     name = name || this.defaultItem
-    //     element(by.model('header.search')).sendKeys(name)
-    // }
+   // searchItem(name: string | null) {
+   //     name = name || this.defaultItem
+   //     element(by.model('header.search')).sendKeys(name)
+   // }
 
 }
